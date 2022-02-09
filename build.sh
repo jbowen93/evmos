e36a80a65311586ffe9cebbce825a97e00e6a620
Josh Bowen <jskybowen@gmail.com>
2022-02-09 11:45:19 -1000

    Add working build script and remove tarball

--- build.sh
+++ build.sh
@@ -1,25 +1,36 @@
 #!/bin/sh
 
 WORKDIR=$PWD
-
-# Get ethermint
-git clone git@github.com:celestiaorg/optimint.git
-cd optimint
-git checkout 939aa77
-rm -rf .git
-go mod tidy -compat=1.17
-cd $WORKDIR
+rm -rf optimint ethermint
 
 # Get Optimint
-git clone git@github.com:jbowen93/ethermint.git
-cd ethermint
-git checkout jbowen93-optimint
+git clone git@github.com:celestiaorg/optimint.git -q
+echo "cloned optimint"
+cd optimint
+git checkout 939aa77 -q
+echo "checked out 939aa77 commit"
 rm -rf .git
 go mod tidy -compat=1.17
+echo "finished tidying optimint"
+cd $WORKDIR
+
+# Get ethermint
+git clone git@github.com:jbowen93/ethermint.git -q
+echo "cloned ethermint"
+cd ethermint
+git checkout jbowen93-optimint -q
+echo "checked out jbowen93-optimint branch"
+rm -rf .git
+go mod edit -replace=github.com/celestiaorg/optimint=../optimint
+go mod tidy -compat=1.17
+echo "finished tidying ethermint"
 cd $WORKDIR
 
 # Cleanup
+go mod edit -replace=github.com/celestiaorg/optimint=./optimint
+go mod edit -replace=github.com/tharsis/ethermint=./ethermint
 go mod tidy -compat=1.17
+echo "finished tidying evmos"
 
 # Docker build
 docker buildx build --platform linux/arm64 -f docker/debug.Dockerfile -t ghcr.io/jbowen93/evmos:testing-debug-arm64 .
