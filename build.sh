2c96c14b9ad44489d27c7c11aae725f972997e4d
Josh Bowen <jskybowen@gmail.com>
2022-02-09 12:55:56 -1000

    working build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DOCKER_TAG=$1
+
 WORKDIR=$PWD
 rm -rf optimint ethermint
 
@@ -10,7 +12,7 @@ cd optimint
 git checkout 939aa77 -q
 echo "checked out 939aa77 commit"
 rm -rf .git
-go mod tidy -compat=1.17
+go mod tidy -compat=1.17 -e
 echo "finished tidying optimint"
 cd $WORKDIR
 
@@ -22,15 +24,15 @@ git checkout jbowen93-optimint -q
 echo "checked out jbowen93-optimint branch"
 rm -rf .git
 go mod edit -replace=github.com/celestiaorg/optimint=../optimint
-go mod tidy -compat=1.17
+go mod tidy -compat=1.17 -e
 echo "finished tidying ethermint"
 cd $WORKDIR
 
 # Cleanup
 go mod edit -replace=github.com/celestiaorg/optimint=./optimint
 go mod edit -replace=github.com/tharsis/ethermint=./ethermint
-go mod tidy -compat=1.17
+go mod tidy -compat=1.17 -e
 echo "finished tidying evmos"
 
 # Docker build
-docker buildx build --platform linux/arm64 -f docker/debug.Dockerfile -t ghcr.io/jbowen93/evmos:testing-debug-arm64 .
+docker buildx build --platform linux/arm64 -f docker/debug.Dockerfile -t ghcr.io/jbowen93/evmos:$DOCKER_TAG .
